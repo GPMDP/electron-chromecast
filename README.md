6277e5b05d2ff65c6a338d6382a113757c5ae78b
Jesse Ostrander <jesse.ostrander@gmail.com>
2016-05-02 22:37:03 -0400

    ApiConfig + Update Readme

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 # electron-chromecast
 An implementation of the chrome.cast API in electron
 
-**TODO:** All the TODO's in code
-**TODO:** The `chrome.cast.media` API
-**TODO:** The `chrome.cast.media.timeout` API
-**TODO:** The `chrome.cast.timeout` API
+## TODO
+- [ ] All the TODO's in code
+- [ ] The `chrome.cast.media` API
+- [ ] The `chrome.cast.media.timeout` API
+- [ ] The `chrome.cast.timeout` API
 
 **TODO *(But totally not needed)*:** The `chrome.cast.games` API
--- src/cast/_Classes/ApiConfig.js
+++ src/cast/_Classes/ApiConfig.js
@@ -1,5 +1,11 @@
 // TODO: https://developers.google.com/cast/docs/reference/chrome/chrome.cast.ApiConfig
 
 export default class ApiConfig {
-
+  constructor(sessionRequest, sessionListener, receiverListener, opt_autoJoinPolicy, opt_defaultActionPolicy) {
+    this.sessionRequest = sessionRequest;
+    this.sessionListener = sessionListener;
+    this.receiverListener = receiverListener;
+    this.autoJoinPolicy = opt_autoJoinPolicy;
+    this.defaultActionPolicy = opt_defaultActionPolicy;
+  }
 }
